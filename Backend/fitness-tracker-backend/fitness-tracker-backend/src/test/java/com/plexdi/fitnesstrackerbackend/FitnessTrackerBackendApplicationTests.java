32f73d3a19f2a1f2785b714e4ba5a35ed68d6a2b
Plexdi <senpai200798@gmail.com>
2025-02-24 20:38:26 +0000

    added backend

---
+++ Backend/fitness-tracker-backend/fitness-tracker-backend/src/test/java/com/plexdi/fitnesstrackerbackend/FitnessTrackerBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.plexdi.fitnesstrackerbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessTrackerBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
